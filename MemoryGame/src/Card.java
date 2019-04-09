d1b8221b3594c8c731757e17c81da6f8dfca3b34
Naoya Hayashi <nhayashi@sfu.ca>
2019-04-09 23:01:43 +0900

    TestCommit #2

--- MemoryGame/src/Card.java
+++ MemoryGame/src/Card.java
@@ -1,11 +1,13 @@
 /**
    Card class stores data necessary for cards in this memory game. <br>
    This class incorporates with the Server but not with the Client. <br>
-   
+
    @author Naoya Hayashi
    @date March 27th, 2013
    @version 1.0
 */
+int dummyCard = 555;
+
 public class Card{
    // this boolean value was not used after all
    // since I disabled the buttons is cards are left open
@@ -15,7 +17,7 @@ public class Card{
    // value means the number assigned to the image files
    // For my program, the number (value) ranges from 1 to 6.
    int value;
-   
+
    /**
          Default Construcor without parameters.
       */
@@ -23,37 +25,36 @@ public class Card{
       isOpen = false;
       value = 0;
    }
-   
-   
+
+
    /**
          Constructor with one parameter, value of the card. <br>
-         
+
          @param val value of the card
       */
    public Card(int val){
       isOpen = false;
       value = val;
    }
-   
-   
+
+
    /**
          Returns value of a card. <br>
-      
+
          @return value of the card
       */
    public int getValue(){
       return value;
    }
-   
-   
+
+
    /**
          Sets a value to this card. <br>
-   
+
          @param val value of the card
       */
    public void setValue(int val){
       value = val;
    }
-   
-}
 
+}
